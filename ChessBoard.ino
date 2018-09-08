98e145dfebb5b9bef387d2e9880efd95fcb362f6
donepel <epel@epelbyte.com>
2018-09-08 12:05:54 -0300

    agrego source de ChessBoard

---
+++ ChessBoard.ino
@@ -0,0 +1,15 @@
+
+/************************************************************
+*****                  ChessBoard                        ****
+*************************************************************/
+// Version 1 Fecha  28 Julio 2018
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
